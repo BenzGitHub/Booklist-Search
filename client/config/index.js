6feeb4941979033bd961d5c151db87815bc9f2ea
BZGitHub <bzmessages@gmail.com>
2019-05-12 23:01:02 -0600

    Added MONGOD_URI

--- client/config/index.js
+++ client/config/index.js
@@ -1,3 +1,3 @@
-const MONGODB_URI = '';
+const MONGODB_URI = 'mongodb://heroku_qlthch88:jhl8jbhmqbe87qusk7garehp1c@ds155596.mlab.com:55596/heroku_qlthch88';
 //const MONGODB_URI = 'mongodb://localhost/books';
 module.exports = MONGODB_URI;
